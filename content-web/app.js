21abaa805566fe5c11c07c2c170dde6b454fbc18
subrata1106 <subrata1106@gmail.com>
2022-09-29 13:48:12 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("1059a8fd-e6e1-4589-a10b-2b999cfc060d");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
